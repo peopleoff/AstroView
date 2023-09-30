a1e2c268ea63deed375cbe124bb09575900b20b7
Ruslan Belyy <ruslanbelyy@gmail.com>
2023-09-30 11:18:47 -0400

    add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", 'nuxt-gtag'],
   supabase: {
     redirect: false,
   },
+  gtag: {
+    id: 'G-9CSPZ17VG5'
+  }
 });
